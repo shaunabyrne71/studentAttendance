dc4eaa6ea0e7ba5fedb1be50bfe820767fb8397f
bva14 <bva14@IREL5GF0PN2.mshome.net>
2018-12-05 14:33:40 +0000

    Push Class Changes

---
+++ src/Student.java
@@ -0,0 +1,18 @@
+/**
+ * Created by meudecc on 15/02/2017.
+ * 05/12/2018 - Shauna Byrne - Updated to Fix asserts
+ */
+public class Student {
+
+    public enum AttendanceGrade {ABSENT, VERY_POOR, POOR, AVERAGE, GOOD, VERY_GOOD};
+
+    public AttendanceGrade getAttendanceGrade(int attendance) {
+        if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
+        if (attendance == 0) return AttendanceGrade.ABSENT;
+        if (attendance < 30) return AttendanceGrade.VERY_POOR;        
+        if (attendance < 70) return AttendanceGrade.AVERAGE;
+        if (attendance < 90) return AttendanceGrade.GOOD;
+        return AttendanceGrade.VERY_GOOD;
+    }
+    
+}
