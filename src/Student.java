9841b1c6f8431add4f3575c8ba3cd704bfb3dd83
bva14 <bva14@IREL5GF0PN2.mshome.net>
2018-12-06 15:11:10 +0000

    Very_Good Score update

--- src/Student.java
+++ src/Student.java
@@ -7,13 +7,12 @@ public class Student {
     public enum AttendanceGrade {ABSENT, VERY_POOR, POOR, AVERAGE, GOOD, VERY_GOOD};
 
     public AttendanceGrade getAttendanceGrade(int attendance) {
-        if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
-        else if (attendance == 0) return AttendanceGrade.ABSENT;
-        else if (attendance < 30) return AttendanceGrade.VERY_POOR;        
-        else if (attendance < 70 && attendance >= 30) return AttendanceGrade.AVERAGE;
-        else if (attendance < 90 && attendance >= 70) return AttendanceGrade.GOOD;
-        else if (attendance >= 90) return AttendanceGrade.VERY_GOOD;
-        return AttendanceGrade.VERY_GOOD;
+    	 if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
+    	 if (attendance == 0) return AttendanceGrade.ABSENT;
+         if (attendance < 30) return AttendanceGrade.VERY_POOR;        
+         if (attendance < 70) return AttendanceGrade.AVERAGE;
+         if (attendance < 90) return AttendanceGrade.GOOD;
+         return AttendanceGrade.VERY_GOOD;
     }
     
 }
