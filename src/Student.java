bb2ff5f704fe75e5558e251bdaa1e27b90813f57
bva14 <bva14@IREL5GF0PN2.mshome.net>
2018-12-06 14:58:00 +0000

    Grade Updates

--- src/Student.java
+++ src/Student.java
@@ -10,8 +10,8 @@ public class Student {
         if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
         if (attendance == 0) return AttendanceGrade.ABSENT;
         if (attendance < 30) return AttendanceGrade.VERY_POOR;        
-        if (attendance < 70) return AttendanceGrade.AVERAGE;
-        if (attendance < 90) return AttendanceGrade.GOOD;
+        if (attendance < 70 && attendance >= 30) return AttendanceGrade.AVERAGE;
+        if (attendance < 90 && attendance >= 70) return AttendanceGrade.GOOD;
         return AttendanceGrade.VERY_GOOD;
     }
     
