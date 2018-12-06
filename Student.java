502df124e778e9449a21d25ba955132e9ea69da1
bva14 <bva14@IREL5GF0PN2.mshome.net>
2018-12-06 16:02:20 +0000

    Update

--- Student.java
+++ Student.java
@@ -9,19 +9,24 @@ public class Student {
 	};
 
 	public AttendanceGrade getAttendanceGrade(int attendance) {
+		System.out.println("attendance = " + attendance);
 		if (attendance < 0 || attendance > 100) {
 			throw new IllegalArgumentException();
 		}
 		if (attendance == 0) {
+			System.out.println("attendance == 0");
 			return AttendanceGrade.ABSENT;
 		}
 		if (attendance < 30) {
+			System.out.println("attendance < 30");
 			return AttendanceGrade.VERY_POOR;
 		}
 		if (attendance < 70) {
+			System.out.println("attendance < 70");
 			return AttendanceGrade.AVERAGE;
 		}
 		if (attendance < 90) {
+			System.out.println("attendance < 90");
 			return AttendanceGrade.GOOD;
 		}
 		return AttendanceGrade.VERY_GOOD;
